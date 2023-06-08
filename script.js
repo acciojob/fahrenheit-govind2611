31126032b7ae842c56f55237813e70f705661be0
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-08 20:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function toFahrenheit(celsius) {
+  if (celsius < -273.15 || celsius > 1.8e9) {
+    throw new Error('Temperature value is out of range');
+  }
+
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  return fahrenheit.toFixed(2);
+}
